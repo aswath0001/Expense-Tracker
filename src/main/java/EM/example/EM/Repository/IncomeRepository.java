1e58b667d1ea0348c56d2ff8890822b8c47e6493
aswath0001 <rsaswaths38@gmail.com>
2025-08-18 15:34:29 +0530

    Added getting expenses for filtering by date range

--- src/main/java/EM/example/EM/Repository/IncomeRepository.java
+++ src/main/java/EM/example/EM/Repository/IncomeRepository.java
@@ -3,5 +3,10 @@ package EM.example.EM.Repository;
 import EM.example.EM.Entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface IncomeRepository extends JpaRepository<Income,Long> {
+   
 }
+
