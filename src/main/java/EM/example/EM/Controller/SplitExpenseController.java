642ede2266f2b77bd96f32d4538814f0f1ca1f41
aswath0001 <rsaswaths38@gmail.com>
2025-08-20 17:42:26 +0530

    Added user features

--- src/main/java/EM/example/EM/Controller/SplitExpenseController.java
+++ src/main/java/EM/example/EM/Controller/SplitExpenseController.java
@@ -6,7 +6,6 @@ import EM.example.EM.Services.SplitExpense.SplitExpenseService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/EM/example/EM/Controller/UserController.java
+++ src/main/java/EM/example/EM/Controller/UserController.java
@@ -7,7 +7,6 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
